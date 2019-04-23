8e828a985315c757db7f4723bd2783399b05633e
mcarn <mlcarnahan22@gmail.com>
2019-04-23 13:17:31 -0500

    Created a Welcome Page for when the user successfully logs in.  Set up a basic intent to send the user to the welcome page when the login button is pressed.  Add the items to a menu.

--- app/src/main/java/edu/apsu/healthyhelper/MainActivity.java
+++ app/src/main/java/edu/apsu/healthyhelper/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.apsu.healthyhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText et = findViewById(R.id.username_editText);
+        final String userET = et.getText().toString();
+
+        et = findViewById(R.id.password_editText);
+        final String passET = et.getText().toString();
+
+        Button button = findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
