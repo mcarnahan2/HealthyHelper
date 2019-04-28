343bdc9beee12c797803eab501f4bc84651af175
Keenya22 <keenyagilchrist@hotmail.com>
2019-04-27 23:20:20 -0500

    Database set up

---
+++ app/src/main/java/edu/apsu/healthyhelper/Water.java
@@ -0,0 +1,25 @@
+package edu.apsu.healthyhelper;
+
+public class Water {
+    private int water_id;
+    private int bottle_count;
+
+    public Water () {
+    }
+
+    public int getWater_id() {
+        return water_id;
+    }
+
+    public void setWater_id(int water_id) {
+        this.water_id = water_id;
+    }
+
+    public int getBottle_count() {
+        return bottle_count;
+    }
+
+    public void setBottle_count(int bottle_count) {
+        this.bottle_count = bottle_count;
+    }
+}
