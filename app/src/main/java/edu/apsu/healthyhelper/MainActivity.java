d96514ed7f02e9e2cfdd3fc4d5f6ca31cf389dbb
mcarn <mlcarnahan22@gmail.com>
2019-04-29 10:35:04 -0500

    Working radio buttons

--- app/src/main/java/edu/apsu/healthyhelper/MainActivity.java
+++ app/src/main/java/edu/apsu/healthyhelper/MainActivity.java
@@ -5,16 +5,20 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int age;
     private int height;
     private int weight;
+    private float calories;
+    private String gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView tv = findViewById(R.id.error_textView);
         tv.setVisibility(View.INVISIBLE);
+
+        RadioGroup rg = findViewById(R.id.gender_radioGroup);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i == R.id.female_radioButton){
+                    //Female
+                    gender = "f";
+                    Log.i("Gender", "Female");
+                } else if (i == R.id.male_radioButton) {
+                    //Male
+                    gender = "m";
+                    Log.i("Gender", "Male");
+                }
+            }
+        });
 
         Button button = findViewById(R.id.login_button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -49,8 +69,11 @@ public class MainActivity extends AppCompatActivity {
                     weight = Integer.parseInt(weightET);
                 }
 
-
-
+                if(gender.equals("f")){
+                    calories = (float) ((10 * (float) weight + 6.25 * (float) height - 5 * (float) age -161));
+                } else {
+                    calories = (float) ((10 * (float) weight + 6.25 * (float) height - 5 * (float) age + 5));
+                }
 
                 if(nameET.equals("") || ageET.equals("") || heightET.equals("") || weightET.equals("")) {
                     tv.setText("Please make sure all fields are filled in.");
@@ -61,12 +84,6 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
 
                     float bmi = (float) (weight * 0.45) / (float) (Math.pow((height * 0.025), 2)) ;
-
-                    //Female
-                    //float calories = (float) ((10 * (float) weight + 6.25 * (float) height - 5 * (float) age -161));
-
-                    //Male
-                    float calories = (float) ((10 * (float) weight + 6.25 * (float) height - 5 * (float) age + 5));
 
                     SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
                     SharedPreferences.Editor editor = preferences.edit();
