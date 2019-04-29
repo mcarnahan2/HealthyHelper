0280743aa48ca20f00691e7631ead0d14acf1824
mcarn <mlcarnahan22@gmail.com>
2019-04-28 22:39:30 -0500

    Added health links

--- app/src/main/java/edu/apsu/healthyhelper/WelcomeActivity.java
+++ app/src/main/java/edu/apsu/healthyhelper/WelcomeActivity.java
@@ -1,9 +1,14 @@
 package edu.apsu.healthyhelper;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class WelcomeActivity extends MenuActivity {
@@ -21,6 +26,41 @@ public class WelcomeActivity extends MenuActivity {
 
         CalendarView calendarView =  findViewById(R.id.calendarView);
 
+        findViewById(R.id.webmd_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.webmd.com/"));
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.vandy_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.vanderbilthealth.com/"));
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.nih_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.nih.gov/"));
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.mayo_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mayoclinic.org/"));
+                startActivity(intent);
+            }
+        });
 
 
 
