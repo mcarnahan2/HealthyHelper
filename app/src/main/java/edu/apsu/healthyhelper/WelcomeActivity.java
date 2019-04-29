8c7efe3b50007911554ec6b8d6b0d633284a1e82
Keenya22 <keenyagilchrist@hotmail.com>
2019-04-29 05:30:10 -0500

    custom calendar

--- app/src/main/java/edu/apsu/healthyhelper/WelcomeActivity.java
+++ app/src/main/java/edu/apsu/healthyhelper/WelcomeActivity.java
@@ -1,17 +1,33 @@
 package edu.apsu.healthyhelper;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class WelcomeActivity extends MenuActivity {
+
+    CompactCalendarView compactCalendarView;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMM- yyy", Locale.getDefault());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +40,35 @@ public class WelcomeActivity extends MenuActivity {
 
         tv.setText("Hello " + name);
 
-        CalendarView calendarView =  findViewById(R.id.calendarView);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
+
+        compactCalendarView = findViewById(R.id.compactcalendar_view);
+        compactCalendarView.setUseThreeLetterAbbreviation(true);
+
+        Event event = new Event(Color.RED,1556460000000L, "Teacher's" +
+                "Professional Day"  );
+        compactCalendarView.addEvent(event);
+
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+
+                if (dateClicked.toString().compareTo("Sun Apr 28 09:00:00 AST 2019") == 0) {
+                    Toast.makeText(context, "Teacher's Professional Day", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "No events", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
+            }
+        });
+
 
         findViewById(R.id.webmd_button).setOnClickListener(new View.OnClickListener() {
             @Override
